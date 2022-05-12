dd976cf06bad0c5777f05edca28675d94a97d9d0
Antony Otero <antonyotero.nyc@gmail.com>
2022-05-12 10:25:00 -0400

    Add landing section

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import logo from '../public/logo.png'
+import logo from '../public/logo.svg'
+import logoLg from '../public/logo-lg.svg'
 import githubIcon from '../public/github-original.svg'
 import linkedinIcon from '../public/linkedin-plain.svg'
 import codepenIcon from '../public/codepen-plain.svg'
@@ -13,7 +14,56 @@ export default function Home() {
         <link rel="icon" href="/favicon.png" />
       </Head>
 
-      <header className='absolute m-[30px]'>
+      <header className='fixed top-0 flex justify-between py-7 px-6 w-full bg-white text-theme-black z-10'>
+        <a href='#landing'>
+          <Image src={logo} />
+        </a>
+        <nav>
+          <ul className='flex text-sm'>
+            <li><a href='#work' className='px-1.5 mr-2 align-middle'>Work</a></li>
+            <li><a href='#about' className='px-1.5 mr-2 align-middle'>About</a></li>
+            <li><a href='#contact' className='px-1.5 align-middle'>Contact</a></li>
+          </ul>
+        </nav>
+      </header>
+
+      <main className='px-2.5 text-theme-black'>
+        <section id='landing' className='h-screen pt-20 bg-white'>
+          <div className='py-8'>
+            <Image src={logoLg} />
+          </div>
+          <p className='max-w-[232px] mx-auto mb-8 text-center'>
+            <span className='font-bold'>Hi, I’m Antony Otero.</span><br/>
+            A Frontend Developer focused on building pixel-perfect, responsive sites with modern web technologies. Interested in crafting secure, performant, and accessible interactive experiences.
+          </p>
+          <div className='btn-group | flex justify-center mb-16 text-xs'>
+            <a href='#work' className='py-3 px-5 mr-5 text-white bg-theme-black border rounded-md border-theme-black'>View Work</a>
+            <a href='#' className='py-3 px-5 border rounded-md border-theme-black'>Contact Me</a>
+          </div>
+          <nav>
+            <ul className='flex justify-center text-sm underline'>
+              <li><a href='' className='mr-8'>GitHub</a></li>
+              <li><a href='' className='mr-8'>LinkedIn</a></li>
+              <li><a href=''>Twitter</a></li>
+            </ul>
+          </nav>
+        </section>
+        <section id='work' className='h-screen bg-green-500'>
+
+        </section>
+        <section id='about' className='h-screen bg-blue-500'>
+
+        </section>
+        <section id='contact' className='h-screen bg-red-500'>
+
+        </section>
+      </main>
+
+      <main>
+
+      </main>
+
+      {/* <header className='absolute m-[30px]'>
         <Image src={logo} />
       </header>
 
@@ -49,7 +99,7 @@ export default function Home() {
         <p className='mb-10 font-bold animate-pulse'>
           This site is currently under construction - 2022
         </p>
-      </footer>
+      </footer> */}
     </>
   )
 }
